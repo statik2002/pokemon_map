986a2865e3c50ab872e3f83af439128a722f2409
statik2002 <statik2002@mail.ru>
2022-10-10 11:37:58 +0800

    add Pokemon.image model field.
    Добавил изображение покемона в модель Pokemon

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 class Pokemon(models.Model):
     title = models.CharField('Наименование покемона', max_length=200)
+    image = models.ImageField('Изображение покемона', upload_to='media/pokemons_images', blank=True, null=True)
 
     class Meta:
         verbose_name = 'Покемон'
